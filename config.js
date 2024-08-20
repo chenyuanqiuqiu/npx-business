f74cc1854b3226d750cac5e81f1ab6c37370fe89
chenyuanqiuqiu <1500995053@qq.com>
2024-08-20 17:00:01 +0800

    修改签到页面

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  version: "14.16.2",
+  version: "5.4.6",
   note: "删除商品详情图片之间的间隙", // 这个为版本描述，无需修改
   subDomain: "chenabcdfg", // 此处改成你自己的专属域名。什么是专属域名？请看教程 https://www.it120.cc/help/qr6l4m.html
   merchantId: 951, // 商户ID，可在后台工厂设置-->商户信息查看
